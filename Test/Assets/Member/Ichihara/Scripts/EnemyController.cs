7bce61c0f9b1c157c9681ef2f819535b0d97f826
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-10-14 10:11:40 +0900

    Create EnemyController
    エネミーをコントロールするスクリプトを作成した。

---
+++ Test/Assets/Member/Ichihara/Scripts/EnemyController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// エネミーの種類
+/// </summary>
+public enum EnemyType
+{
+    None,
+    Enamy,
+    Debri,
+}
+
+public class EnemyController : MonoBehaviour
+{
+    // このスクリプトがアタッチされているオブジェクト
+    private GameObject _obj = null;
+    // オブジェクトのエネミーの種類
+    private EnemyType Type = EnemyType.None;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _obj = GameObject.Find(name);
+        if (tag == "Enemy") { Type = EnemyType.Enamy; }
+        else if (tag == "Debri") { Type = EnemyType.Debri; }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
