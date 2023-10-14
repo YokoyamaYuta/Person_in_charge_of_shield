7f2fe51c3410298157c75f02581054f5e8fcc65e
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-10-14 16:15:10 +0900

    Create Shell.cs
    宇宙海賊が発砲する弾の機能を作成、実装した。

---
+++ Test/Assets/Member/Ichihara/Scripts/Shell.cs
@@ -0,0 +1,45 @@
+﻿using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+    // Shell の移動速度
+    [SerializeField, Range(0.0f, 19.2f)]
+    private float _shellSpeed = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveShell(this.GetCancellationTokenOnDestroy()).Forget();
+    }
+
+    private async UniTask MoveShell(CancellationToken token)
+    {
+        bool isCanceled = false;
+        while (isCanceled == false)
+        {
+            try
+            {
+                await UniTask.WaitForSeconds(Time.deltaTime);
+                transform.Translate(Vector3.up * _shellSpeed * Time.deltaTime);
+            }
+            catch(MissingReferenceException)
+            {
+                break;
+            }
+        }
+    }
+
+    public void GenerateShell(Transform parent)
+    {
+        Instantiate(gameObject, parent);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
