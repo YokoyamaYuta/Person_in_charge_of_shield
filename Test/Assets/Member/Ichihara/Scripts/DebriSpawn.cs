83189d2a49d425df79adec0491919afc273ea87a
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-10-16 12:48:01 +0900

    Update DebriSpawn.cs
    デブリのランダム生成機能が完成した。

--- Test/Assets/Member/Ichihara/Scripts/DebriSpawn.cs
+++ Test/Assets/Member/Ichihara/Scripts/DebriSpawn.cs
@@ -1,19 +1,24 @@
 ﻿using Cysharp.Threading.Tasks;
-using System.Collections;
-using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class DebriSpawn : SingletonMonoBehaviour<DebriSpawn>
 {
+    // デブリオブジェクト
     [SerializeField]
     private GameObject _debri = null;
+    // デブリをスポーンする座標を決めるカーブ
     [SerializeField]
     private AnimationCurve _debriSpawnCurve = null;
+    // デブリの種類を決めるカーブ
     [SerializeField]
     private AnimationCurve _debriTypeCurve = null;
 
+    // デブリがスポーンする間隔
+    [SerializeField]
+    private float _spawnInterval = 0.0f;
+    // デブリ
     private float[] _debriSpawnCurveTimes = { };
 
     // Start is called before the first frame update
@@ -25,16 +30,11 @@ public class DebriSpawn : SingletonMonoBehaviour<DebriSpawn>
         {
             _debriSpawnCurveTimes[i] = _debriSpawnCurve.keys[i].time;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        CallRandomSpawnDebris();
     }
 
     /// <summary>
-    /// AnimationCurve のキーを設定する
+    /// _debriSpawnCurve のキーを設定する
     /// </summary>
     private void CreateKeys()
     {
@@ -42,16 +42,49 @@ public class DebriSpawn : SingletonMonoBehaviour<DebriSpawn>
         _debriSpawnCurve.AddKey(1.0f, 1.0f);
     }
 
-    private async UniTask RandomSpawnDebris(CancellationToken token)
+    /// <summary>
+    /// RandomSpawnDebris を呼び出す
+    /// </summary>
+    private void CallRandomSpawnDebris()
     {
-
+        RandomSpawnDebris(this.GetCancellationTokenOnDestroy()).Forget();
     }
 
+    /// <summary>
+    /// デブリをランダムな位置に配置する
+    /// </summary>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    private async UniTask RandomSpawnDebris(CancellationToken token = default)
+    {
+        bool isEndGame = false;
+        while (isEndGame == false)
+        {
+            // TODO: ここにループを止める処理を記述する
+            await UniTask.WaitForSeconds(_spawnInterval, cancellationToken: token);
+            SpawnDebris();
+        }
+    }
+
+    /// <summary>
+    /// デブリを生成する
+    /// </summary>
     private void SpawnDebris()
     {
+        // スクリーンのサイズを Unity の座標に置き直す
+        Vector2 gameViewScale = new Vector2(Screen.width, Screen.height) / 100f;
         float random = _debriSpawnCurve.Evaluate(Random.value);
-        float result = random * (Screen.height / 100.0f);
-        Vector3 spawnPosition = Vector3.right * (Screen.width / 100.0f) + Vector3.up * result;
+        float result = random * gameViewScale.y;
+        // result の値を -5.4 ~ 5.4 の間に正規化
+        float normalizedResult = Mathf.Repeat(result, gameViewScale.y) - gameViewScale.y / 2;
+        Vector3 dummySpawnPosition = Vector3.right * gameViewScale.x / 2 + Vector3.up * normalizedResult;
+        float dummySpawnPositionY = dummySpawnPosition.y;
+        // 限界値を定義
+        Vector3 spawnPosition = new Vector3(dummySpawnPosition.x
+                                          , Mathf.Clamp(dummySpawnPositionY
+                                            , -gameViewScale.y + _debri.transform.localScale.y / 2
+                                            , gameViewScale.y - _debri.transform.localScale.y / 2)
+                                          , dummySpawnPosition.z);
         Instantiate(_debri, spawnPosition, Quaternion.identity);
     }
 }
